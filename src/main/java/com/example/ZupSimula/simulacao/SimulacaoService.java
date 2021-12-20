359c92840640e6098e28e0e967206e16e7c9487c
Debora Souza <deboral.p@hotmail.com>
2021-12-20 11:48:59 -0300

    Criar classe Service

---
+++ src/main/java/com/example/ZupSimula/simulacao/SimulacaoService.java
@@ -0,0 +1,14 @@
+package com.example.ZupSimula.simulacao;
+
+import com.example.ZupSimula.dto.SimulacaoDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class SimulacaoService {
+    private List<SimulacaoDTO>simulacaoDTOS = new ArrayList<>();
+
+
+}
