2d073dbc315ffdaf8e0d4cf1c55167d826eb19b1
Debora Souza <deboral.p@hotmail.com>
2021-12-20 17:12:00 -0300

    Modelar classe ErroDeValidacao

--- src/main/java/com/example/ZupSimula/config/ErroDeValidacao.java
+++ src/main/java/com/example/ZupSimula/config/ErroDeValidacao.java
@@ -1,2 +1,27 @@
-package com.example.ZupSimula.config;public class ErrodeValidacao {
+package com.example.ZupSimula.config;
+
+public class ErroDeValidacao {
+    private String campo;
+    private String mensagem;
+
+    public ErroDeValidacao(String mensagem, String campo) {
+        this.mensagem = mensagem;
+        this.campo = campo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public void setCampo(String campo) {
+        this.campo = campo;
+    }
 }
