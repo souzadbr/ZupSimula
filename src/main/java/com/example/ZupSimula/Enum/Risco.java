8b4c5544309a9c4e355ed5c7c7771d59e833b66d
Debora Souza <deboral.p@hotmail.com>
2021-12-20 11:43:14 -0300

    Modelar Enum 'Risco'

---
+++ src/main/java/com/example/ZupSimula/Enum/Risco.java
@@ -0,0 +1,15 @@
+package com.example.ZupSimula.Enum;
+
+public enum Risco {
+    ALTO(0.025), MEDIO(0.015), BAIXO(0.005);
+
+    private double taxaDeRendimento;
+
+    Risco(double valor){
+        taxaDeRendimento = valor;
+    }
+
+    public double getTaxaDeRendimento() {
+        return taxaDeRendimento;
+    }
+}
---
+++ src/main/java/com/example/ZupSimula/Simulacao/dto/SimulacaoDTO.java
@@ -0,0 +1,61 @@
+package com.example.ZupSimula.Simulacao.dto;
+
+public class SimulacaoDTO {
+    private String email;
+    private String nome;
+    private String cpf;
+    private double valorInvestimento;
+    private int periodoDeAplicacao;
+    private Risco risco;
+
+    public SimulacaoDTO() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getValorInvestimento() {
+        return valorInvestimento;
+    }
+
+    public void setValorInvestimento(double valorInvestimento) {
+        this.valorInvestimento = valorInvestimento;
+    }
+
+    public int getPeriodoDeAplicacao() {
+        return periodoDeAplicacao;
+    }
+
+    public void setPeriodoDeAplicacao(int periodoDeAplicacao) {
+        this.periodoDeAplicacao = periodoDeAplicacao;
+    }
+
+    public Risco getRisco() {
+        return risco;
+    }
+
+    public void setRisco(Risco risco) {
+        this.risco = risco;
+    }
+}
