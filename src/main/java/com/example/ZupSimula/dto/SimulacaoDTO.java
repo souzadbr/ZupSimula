00391518a8d77bc082abc5740e114d4aa7ee901a
Debora Souza <deboral.p@hotmail.com>
2021-12-21 09:53:17 -0300

    Adiconar validacao de CPF

--- src/main/java/com/example/ZupSimula/dto/SimulacaoDTO.java
+++ src/main/java/com/example/ZupSimula/dto/SimulacaoDTO.java
@@ -1,6 +1,7 @@
 package com.example.ZupSimula.dto;
 
 import com.example.ZupSimula.enums.Risco;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
@@ -9,6 +10,7 @@ public class SimulacaoDTO {
     @Email(message = "Email inválido")
     private String email;
     private String nome;
+    @CPF(message = "CPF inválido")
     private String cpf;
     private double valorInvestimento;
     @Min(value = 2,message = "Periodo minimo de aplicação é de 2 meses ")
