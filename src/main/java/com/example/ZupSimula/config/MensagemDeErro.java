448bfa4f26929169ad40edb91cda9d66f4054ebf
Debora Souza <deboral.p@hotmail.com>
2021-12-21 09:32:45 -0300

    Modelar classe MensagemDeErro

--- src/main/java/com/example/ZupSimula/config/MensagemDeErro.java
+++ src/main/java/com/example/ZupSimula/config/MensagemDeErro.java
@@ -1,2 +1,17 @@
-package com.example.ZupSimula.config;public class MensagemDeErro {
+package com.example.ZupSimula.config;
+
+public class MensagemDeErro {
+   private String mensagemDeErro;
+
+    public MensagemDeErro(String mensagemDeErro) {
+        this.mensagemDeErro = mensagemDeErro;
+    }
+
+    public String getMensagemDeErro() {
+        return mensagemDeErro;
+    }
+
+    public void setMensagemDeErro(String mensagemDeErro) {
+        this.mensagemDeErro = mensagemDeErro;
+    }
 }
